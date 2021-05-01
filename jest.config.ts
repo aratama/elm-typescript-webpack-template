6d8a1a6b137356802fd6ef987f54c3be3fc0ab29
aratama <16192627+aratama@users.noreply.github.com>
2021-05-02 02:50:33 +0900

    use import in jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import { InitialOptions } from "@jest/types/build/Config";
+
+export const config: InitialOptions = {
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
 };
+
+export default config;
